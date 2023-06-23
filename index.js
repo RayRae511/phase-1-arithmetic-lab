425701b233b832f6274c0bcf120437ea66d616ce
Raymond <oetaray@gmail.com>
2023-06-23 11:17:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() > 0)
+
+let num3 = 16;
+let num4 = 4;
+const mod = num3 / num4;
+
+const max = (5,10,15,20);
+max.max();
